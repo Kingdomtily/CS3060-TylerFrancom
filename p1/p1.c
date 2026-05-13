7bc8ba95dede711f7bcb7ce28eaaff14c995c640
Tyler Francom <tjfrancom@gmail.com>
2026-05-12 22:31:39 -0600

    added loop and print

--- p1/p1.c
+++ p1/p1.c
@@ -2,8 +2,17 @@
 CS3060 X02 Summer 2026
 Program 1*/
 
-#include <studio.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[]){
-    printf("Program 1 by Tyler Francom");
+int main(int argc, char *argv[]) {
+
+    printf("Program 1 by Tyler Francom\n");
+
+    for (int i = 0; i < argc; i++) {
+        printf("Argument #%d: %s\n", i, argv[i]);
+    }
+
+    printf("Number of arguments printed: %d\n", argc);
+
+    return 0;
 }
