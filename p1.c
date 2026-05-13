cffe55a433284d5c99c0853f4be0f5421da2d54d
Tyler Francom <tjfrancom@gmail.com>
2026-05-12 22:27:59 -0600

    Added print function

--- p1.c
+++
@@ -1,3 +0,0 @@
-/*Tyler Francom
-CS3060 X02 Summer 2026
-Program 1*/
---
+++ p1/p1.c
@@ -0,0 +1,9 @@
+/*Tyler Francom
+CS3060 X02 Summer 2026
+Program 1*/
+
+#include <studio.h>
+
+int main(int argc, char *argv[]){
+    printf("Program 1 by Tyler Francom");
+}
