0ae9e66e7866f9dc270382a65515fe708162253a
Tyler Francom <tjfrancom@gmail.com>
2026-05-29 20:39:16 -0600

    Fixed wording to be more professional, Fiddled with gitignore a bit more (Hopefully it helps)

--- p2/p2.c
+++ p2/p2.c
@@ -5,46 +5,40 @@ Program 2*/
 /*If you ever have a student who is struggling with coding, refer them to geeksforgeeks. I used this article to help me understand this project:
 https://www.geeksforgeeks.org/c/fork-system-call/*/
 
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-int main(int argc, char *argv[]){
-    pid_t my_fork; /*Creates the ID variable*/
+int main(int argc, char *argv[]) {
+    pid_t my_fork;
     int status;
-
     my_fork = fork();
-
-    /*need to be sure the fork worked correctly*/
-    if (my_fork < 0){
-        printf("Fork failed!");
+    if (my_fork < 0) {
+        printf("Fork failed!\n");
         return 1;
     }
-    else if (my_fork == 0){
-        if(argc == 1){
-            printf("Child was started. No arguments were provided. Terminating child o7. \n");
+    else if (my_fork == 0) {
+        if (argc == 1) {
+            printf("CHILD started. No arguments provided. Terminating child.\n");
             return 0;
         }
         else if (argc == 2) {
-            printf("Child was started. One argument provided. Lets run execlp() bois. \n");
+            printf("CHILD started. One argument provided. Calling execlp(), never to return (sniff)\n");
             execlp(argv[1], argv[1], NULL);
             perror("execlp failed");
             return 1;
         }
-        
-        else if (argc > 2){
-            printf("Child was started. more than one argument given. Running execvp(). \n");
+        else {
+            printf("CHILD started. More than one argument provided. Calling execvp(), never to return (sniff)\n");
             execvp(argv[1], &argv[1]);
-            perror("execlp failed");
+            perror("execvp failed");
             return 1;
         }
-        
     }
-    else{
-        printf("Pretty sure this is parent. Waiting for dumb ol'%d \n", my_fork);
+    else 
+        printf("PARENT started, now waiting for process ID#%d\n", my_fork);
         waitpid(my_fork, &status, 0);
-        printf("We can keep going now.");
+        printf("PARENT resumed. Child exit code of %d. Now terminating parent\n", WEXITSTATUS(status));
         return 0;
     }
-}
