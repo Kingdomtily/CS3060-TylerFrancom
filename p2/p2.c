b97ffd5bae22246550a508f0c3d39c7176ac5c19
Tyler Francom <tjfrancom@gmail.com>
2026-05-29 19:43:42 -0600

    Haha, I'm Smrt

--- p2/p2.c
+++ p2/p2.c
@@ -19,5 +19,14 @@ int main(){
     if (my_fork < 0){
         printf("Fork failed!");
         return 1;
+
+    elif (my_fork == 0){
+        printf("I am your secret child. My ID is %d", getpid());
+        return 1;
+    }
+    else{
+        printf("Well why would this be happening");
+        return 1;
+    }
     }
 }
