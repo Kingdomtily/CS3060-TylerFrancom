dfa9e0d2b50ccdc375389b5ff80d85f3c1ccc6e4
Tyler Francom <tjfrancom@gmail.com>
2026-05-29 19:25:48 -0600

    Added initial fork creation, and checking for fail

--- p2.c
+++ p2.c
@@ -2,11 +2,22 @@
 CS3060 X02 Summer 2026
 Program 2*/
 
+/*If you ever have a student who is struggling with coding, refer them to geeksforgeeks. I used this article to help me understand this project:
+https://www.geeksforgeeks.org/c/fork-system-call/*/
+
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/types.h>
 #include<sys/wait.h>
 
 int main(){
-    
+    pid_t my_fork; /*Creates the ID variable*/
+
+    my_fork = fork();
+
+    /*need to be sure the fork worked correctly*/
+    if (my_fork < 0){
+        fpritnf("Fork failed!");
+        return 1
+    }
 }
