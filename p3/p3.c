859455411b6c03f03a7cb6cb4b316ed18d467f84
Tyler Francom <tjfrancom@gmail.com>
2026-06-04 18:22:46 -0600

    Created a function to factor numbers, and a test for it. Testing now.

--- p3/p3.c
+++ p3/p3.c
@@ -1,3 +1,27 @@
 /*Tyler Francom
 CS3060 X02 Summer 2026
 Program 3*/
+
+#include <pthread.h>
+#include <stdio.h>
+
+
+void factor(number){
+    while (number%2 ==0){
+        printf("%d", 2);
+        number = number/2;
+    }
+    for (int i =3; i*i <=n; i = i+2){
+        while (n % i == 0){
+            printf("%d", i);
+            number = number/i;
+        }
+    }
+    if (n > 2)
+}
+
+int main(){
+    int test = 315;
+    factor(test);
+    return 0;
+}
