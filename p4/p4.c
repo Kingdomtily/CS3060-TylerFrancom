cb7091f3e912acde7ebb953d520996c4f4820104
Tyler Francom <tjfrancom@gmail.com>
2026-06-09 21:42:25 -0600

    Testing scanf and taking in the .txt

--- p4/p4.c
+++ p4/p4.c
@@ -2,9 +2,16 @@
 CS3060 X02 Summer 2026
 Program 4*/
 
+#include <stdio.h>
+
 #define MAX_PROCESSES 100
 
 int arrivalTime[MAX_PROCESSES];
 int burstTime[MAX_PROCESSES];
 int processCount = 0;
 
+while(scanf("%d%d", &arrivalTime[processCount], &burstTime[processCount]) == 2){
+    processCount++;
+}
+
+printf("Read %d processes\n", processCount)
