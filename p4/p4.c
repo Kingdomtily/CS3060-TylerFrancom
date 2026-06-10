b7a2a23a0a3d2f17c4090f198e624f512ee3aabe
Tyler Francom <tjfrancom@gmail.com>
2026-06-09 22:37:16 -0600

    Realized I initialized the wrong thing. Didn't add up and kept changing the value. Fixed now, I think.

--- p4/p4.c
+++ p4/p4.c
@@ -14,14 +14,16 @@ void fcfs(int arrivalTime[], int burstTime[], int n){
     int clock = 0;
     int firstRun[MAX_PROCESSES];
     int finish[MAX_PROCESSES];
-    double responseTime = 0;
-    double turnaroundTime = 0;
-    double waitTime = 0;
+    double totalResponse = 0;
+    double totalTurnaround = 0;
+    double totalWait = 0;
 
-        for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++){
         firstRun[i] = -1;
     }
-    for(int i = 0; i < n; i++){
+
+    for(int i = 0; i < n; i++)
+    {
         if(clock < arrivalTime[i]){
             clock = arrivalTime[i];
         }
@@ -29,22 +31,34 @@ void fcfs(int arrivalTime[], int burstTime[], int n){
         clock += burstTime[i];
         finish[i] = clock;
     }
-    for(int i = 0; i < n; i++)
-{
-    responseTime = firstRun[i] - arrivalTime[i];
-    turnaroundTime = finish[i] - arrivalTime[i];
-    waitTime = turnaroundTime - burstTime[i];
+
+    for(int i = 0; i < n; i++){
+        int responseTime =
+            firstRun[i] - arrivalTime[i];
+        int turnaroundTime =
+            finish[i] - arrivalTime[i];
+        int waitTime =
+            turnaroundTime - burstTime[i];
+        totalResponse += responseTime;
+        totalTurnaround += turnaroundTime;
+        totalWait += waitTime;
+    }
+    printf("Avg. Resp.:%.2f, Avg. T.A.:%.2f, Avg. Wait:%.2f\n",
+        totalResponse / n,
+        totalTurnaround / n,
+        totalWait / n
+    );
 }
-}
+
+
 int main(void){
 while(scanf("%d%d", &arrivalTime[processCount], &burstTime[processCount]) == 2)
 {
     processCount++;
 }
 
-
-fcfs(arrivalTime, burstTime, processCount);
 printf("First come, First serve \n");
+fcfs(arrivalTime, burstTime, processCount);
 printf("Shortest Job First \n");
 printf("Shortest Remaining Time First\n");
 printf("Rount Robin with Time Quantum of 100 \n");
