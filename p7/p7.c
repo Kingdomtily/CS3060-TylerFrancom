f85549881621d5036daf7b0e4844cbdf520b5a83
Tyler Francom <tjfrancom@gmail.com>
2026-07-29 15:43:43 -0600

    Sort array created

--- p7/p7.c
+++ p7/p7.c
@@ -11,6 +11,26 @@ void copyArray(const int source[], int destination[], int count)
     }
 }
 
+void sortArrayy(int array[], int count)
+{
+    for (int i=0; i < count -1; i++)
+    {
+        int smallestIndex = i;
+        for (int j = i + 1; j < count; j++)
+        {
+            if (array[j] < array[smallestIndex])
+            {
+                smallestIndex = j;
+            }
+        }
+        if (smallestIndex != i)
+        {
+            int temp = array[i];
+            array[i] = array[smallestIndex];
+            array[smallestIndex] = temp;
+        }
+    }
+}
 int fcfs(int requests[], int count);
 int sstf(int requests[], int count);
 int look(int requests[], int count);
