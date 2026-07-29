f2b41f59aef16d94e4d7d174d5922d3e39c09060
Tyler Francom <tjfrancom@gmail.com>
2026-07-29 15:30:10 -0600

    Made a function for copying the values from one array into another

--- p7/p7.c
+++ p7/p7.c
@@ -3,6 +3,14 @@
 
 #define MAX_REQUESTS 100
 
+void copyArray(const int source[], int destination[], int count)
+{
+    for (int i=0; i< count; i++)
+    {
+        destination[i] = source[i];
+    }
+}
+
 int fcfs(int requests[], int count);
 int sstf(int requests[], int count);
 int look(int requests[], int count);
