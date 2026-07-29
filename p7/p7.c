3267a7f8ab21f15570525c440f21672f980fdf42
Tyler Francom <tjfrancom@gmail.com>
2026-07-29 15:16:25 -0600

    Basic initialization babyyyy

---
+++ p7/p7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_REQUESTS 100
+
+int fcfs(int requests[], int count);
+int sstf(int requests[], int count);
+int look(int requests[], int count);
+int clook(int requests[], int count);
+
+int main(void)
+{
+    return 0;
+}
