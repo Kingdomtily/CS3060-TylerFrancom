3b26901599f4c615502e344b3108d42ccc28848b
Tyler Francom <tjfrancom@gmail.com>
2026-07-29 20:02:48 -0600

    get some includes in here

--- p8/p8.c
+++ p8/p8.c
@@ -1,3 +1,13 @@
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define _GNU_SOURCE
+
+
 int main(int argc, char *argv[])
 {
     return 0;
